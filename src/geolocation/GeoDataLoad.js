fd634b2371b2dcb10af8189f07dcd4fc69f133a8
Mariella DE CROUY CHANEL <mariella.decrouychanel@uni.lu>
2020-10-20 18:11:46 +0200

    no message

--- src/geolocation/GeoDataLoad.js
+++ src/geolocation/GeoDataLoad.js
@@ -3,19 +3,6 @@ import * as d3 from 'd3'
 import {Link} from 'react-router-dom'
 import { Segment, Button, Divider, Dimmer, Loader, Message, Container, Form, Select, Header } from 'semantic-ui-react'
 
-
-const removeSpaces = (d) => { 
-    Object.keys(d).forEach(function(origProp) {
-        const noSpace = replaceAll(origProp," ", "_");
-        d[noSpace] = d[origProp];
-    });
-    return d;
-}
-
-const replaceAll = (string, search, replace) => {
-    return string.split(search).join(replace);
-}
-
 /*const tooltipOptions = [
     { key: 'bz', value: 'bz', text: 'ersatz_id' },
     { key: 'bj', value: 'bj', text: 'rawPoB' },
@@ -29,27 +16,25 @@ const GeoDataLoad = () => {
 
    const fetchData = () => {
         d3.csv(process.env.PUBLIC_URL + "anon_rosterm_id.csv").then(dataset => {
-            const cols = dataset.columns.length
             setData(dataset)
-            console.log(dataset)
           });
       
         d3.json(process.env.PUBLIC_URL + "world_1914.json").then(usermap => {
             setUsermap(usermap)
-            console.log(usermap)
         })
     }
     
-      useEffect(() => {
-        fetchData()
-      }, [])
+    useEffect(() => {
+    fetchData()
+    }, [])
 
     
 
   return (data!=null && usermap !== null) ? (
            
+    <>
     <Container text>
-   
+    <Header size='small'>Upload data and basemap</Header>
     <Button icon='upload' content = 'Upload dataset (csv)' />
     <Button.Group>
     <Button>Load default basemap</Button>
@@ -57,7 +42,7 @@ const GeoDataLoad = () => {
     <Button>Upload your own</Button>
     </Button.Group>
     <Divider></Divider>
-    <Header size='medium'>Select latitude and longitude</Header>
+    <Header size='small'>Select latitude and longitude</Header>
     <Message>To enable map display, indicate which columns of your dataset describe latitude and longitude.</Message>
     <Form size='mini'>
         <Form.Group widths='equal'>
@@ -72,17 +57,15 @@ const GeoDataLoad = () => {
           placeholder='Longitude'
         />
         </Form.Group>
+        <Form.Button size = 'small'>Update</Form.Button> 
+    </Form>
 
-        <Form.Button size = 'small'>Update</Form.Button>
-        
-        </Form>
-
-        <Divider></Divider>
-        
-        
+    <Divider></Divider>
+    </Container>
+    <Container>
 
     </Container>
-            
+    </>       
             
           ): (<Container>
             <Dimmer active inverted>
---
+++ src/geolocation/GeoExplMap.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as d3 from 'd3'
+import {Delaunay} from 'd3-delaunay';
+import { Tab, Card, List, Table, Divider, Select, Button, Icon, Header, Message, Dropdown, Container, Form} from 'semantic-ui-react'
+
+const GeoExplMap = (props) => {
+
+    
+
+
+
+
+}
